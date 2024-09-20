cfda72dcf29920125e0957d9f8e179d43f0e69b2
jonnytouma <jonnytouma@gmail.com>
2024-09-20 14:58:48 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FoodHubMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
